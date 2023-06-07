558622f26f856f1477413ec1710daa42ab951f40
adilkhansherani <50609114+adilkhansherani@users.noreply.github.com>
2023-06-07 17:56:40 +0530

    added username and password fields

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  double kBorderRadius = 10.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +19,93 @@ class _LoginScreenState extends State<LoginScreen> {
       physics: const BouncingScrollPhysics(),
       padding: const EdgeInsets.symmetric(),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset('lib/svgs/Login_Screen.svg'),
-            ],
+          IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SvgPicture.asset('lib/svgs/Login_Screen.svg'),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 0.5,
+            ), //                work here
+            width: 300,
+            child: const Column(children: [
+              SizedBox(
+                height: 15,
+                child: ColoredBox(color: Color(0x41835486)),
+              ),
+              Text(
+                'Hello',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 15,
+                child: ColoredBox(color: Color.fromARGB(255, 131, 84, 134)),
+              ),
+              Text(
+                "let's see Construction Expense Manager",
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+                child: ColoredBox(color: Color(0x41835486)),
+              ),
+              /**const Text(
+                "username",
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              */
+              TextField(
+                maxLength: 50,
+                decoration: InputDecoration(
+                  //labelText: 'Password', // Optional label text
+                  hintText: 'Username',
+                  /**labelStyle: TextStyle(
+                    decoration: TextDecoration
+                        .underline, // Add underline decoration to the label
+                  ),
+                  */
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color:
+                            Colors.grey), // Define the color of the underline
+                  ),
+                ),
+              ),
+              TextField(
+                maxLength: 50,
+                decoration: InputDecoration(
+                  //labelText: 'Password', // Optional label text
+                  hintText: 'Password',
+                  /**labelStyle: TextStyle(
+                    decoration: TextDecoration
+                        .underline, // Add underline decoration to the label
+                  ),
+                  */
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color:
+                            Colors.grey), // Define the color of the underline
+                  ),
+                ),
+              ),
+            ]),
           ),
         ],
       ),
