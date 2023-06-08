dfb2f24a7176dd34d210808eb7d68c7f92e6684c
adilkhansherani <50609114+adilkhansherani@users.noreply.github.com>
2023-06-09 00:20:39 +0530

    added Top bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,9 +12,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3B9B67),
+        title: const Text(
+          'Baldur',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),
+        ),
+        elevation: 2,
+        actions: const [
+          Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+          SizedBox(width: 9),
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
+        shape: const CircleBorder(),
         onPressed: () {},
         backgroundColor: const Color.fromARGB(255, 0x3B, 0x9B, 0x67),
         child: const Icon(Icons.add),
