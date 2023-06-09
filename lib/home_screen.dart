a3113bb830b2c40443dbb84d57642e1e522e0a3e
adilkhansherani <50609114+adilkhansherani@users.noreply.github.com>
2023-06-09 15:02:44 +0530

    added home page fund container

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -41,7 +41,122 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: const Color.fromARGB(255, 0x3B, 0x9B, 0x67),
         child: const Icon(Icons.add),
       ),
-      bottomNavigationBar: BottomNavigator(),
+      bottomNavigationBar: const BottomNavigator(),
+      body: ListView(physics: const ClampingScrollPhysics(), children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Column(children: [
+            const SizedBox(
+              height: 15,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                width: 350,
+                alignment: Alignment.center,
+                color: const Color(0xff5AB584),
+                padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+                child: Column(
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Today',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Icon(
+                          Icons.arrow_drop_down,
+                          color: Colors.black,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text('Total Balance'),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text('10009625.0 INR'),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      child: Container(
+                        padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+                        color: const Color(0xFF3B9B67),
+                        child: const Row(
+                          //income and expences section
+                          children: [
+                            Column(
+                              //income side
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_downward,
+                                      color: Colors.black,
+                                    ),
+                                    Text(
+                                      'Income',
+                                      style: TextStyle(color: Colors.black),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text('40000'),
+                              ],
+                            ),
+                            Expanded(
+                              //didn't work, work here
+                              child: VerticalDivider(
+                                width: 20,
+                                thickness: 5,
+                                color: Color.fromARGB(255, 247, 22, 22),
+                              ),
+                            ),
+                            Column(
+                              //expense side
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_upward,
+                                      color: Colors.black,
+                                    ),
+                                    Text(
+                                      'expense',
+                                      style: TextStyle(color: Colors.black),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text('38725'),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Row(),
+            const SizedBox(),
+            Container(),
+          ]),
+        ),
+      ]),
     );
   }
 }
